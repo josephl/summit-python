ad68c13a4080c80c88b039e3033f6b94421f4a27
Joseph Lee <joseph@idealist.org>
2015-05-27 14:47:39 -0500

    Add demo script to send SMS message from command_line

---
+++ sms_send_demo.py
@@ -0,0 +1,34 @@
+"""
+Basic demo of summit-python that sends a simple SMS message from the
+command-line.
+"""
+
+import argparse
+
+from summit.rest import SummitRestClient
+
+
+def main():
+    parser = argparse.ArgumentParser(
+        description="Command-line SMS sender using Corvisa's Summit API.")
+    parser.add_argument('--key', required=True,
+                        help="Your application's API key.")
+    parser.add_argument('--secret', required=True,
+                        help="Your application's API key secret.")
+    parser.add_argument('--from', required=True, help=(
+        "Number to send from. Must be an authorized number "
+        "for your Summit app."))
+    parser.add_argument('--to', required=True, help="Recipient of SMS message.")
+    parser.add_argument('--message', required=True,
+                        help="Body of the SMS message.")
+    args = parser.parse_args()
+
+    client = SummitRestClient(account=args.key, token=args.secret)
+    resp, inst = client.messages.create(from_=from_number, to=to_number,
+                                        body=args.message)
+    print 'Responded with code: {}'.format(resp.status_code)
+    print inst
+
+
+if __name__ == '__main__':
+    main()
